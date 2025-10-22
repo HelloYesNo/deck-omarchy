3c238387d8c03b3258a9821dfc2fe5c7cf6cef9e
HelloYesGo <helloyesgo@20014c4e1f8cfb00c3628c30b5e12c83.dsl.pool.telekom.hu>
2025-10-22 23:24:58 +0300

    Minimal-arch-test

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux kitty firefox vim 
+# dnf5 -y remove plasma-workspace plasma-* kde-*
+
+
 
 # Use a COPR Example:
 #
