001eeb18807d8368e839065a70540955bc6863d3
HelloYesNo <38365632+HelloYesNo@users.noreply.github.com>
2025-10-27 17:30:00 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 -y remove plasma-workspace plasma-* kde-*
+# dnf5 -y remove plasma-workspace plasma-* kde-*
 
 # Use a COPR Example:
 #
